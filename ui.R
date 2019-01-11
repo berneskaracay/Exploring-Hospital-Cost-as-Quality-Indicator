953262a1b9163956dc00c0a7e5d37ee8bc6ed4db
Karacay <karacb1@vanderbilt.edu>
2019-01-11 15:02:00 -0600

    selection bar added

--- ui.R
+++ ui.R
@@ -2,29 +2,41 @@
 
 shinyUI(dashboardPage(
   dashboardHeader(title = "Healthcare Cost Dashboard"),
-  dashboardSidebar(tags$blockquote("Select inputs below to filter dashboard."),
-                   selectInput("quality",
-                               label = "Quality",
-                               choices= quality,
-                               selected = 4),
-                   selectInput("state",
-                               label = "State",
-                               choices = states,
-                               selected = "TN",
-                               selectize = TRUE),
-                   selectInput("procedure",
-                               label = "Medical Procedure",
-                               choices = procedure,
-                               selected = "101 - SEIZURES W/O MCC",
-                               selectize = TRUE),
-                   uiOutput("secondSelection",)
-
-),
+  dashboardSidebar(
+    sidebarMenu(
+                h5('by Bernes Karacay', align = 'center'),
+                br(),
+                menuItem("Filter Data and Download", tabName = "cost", icon = icon("database")),
+                menuItem("Connecticut", tabName = "ct", icon = icon("database"))
+                
+    )
+  ),
   
   dashboardBody(
-    fluidRow(
-      box(title = "Healthcare provider list", status = "primary",
-          dataTableOutput("opioid_providers_table"), width = 12)
+    tabItems(
+      tabItem(tabName = "cost",
+        h3("Select inputs below to filter dashboard."),
+        selectInput("quality",
+                    label = "Quality",
+                    choices= quality,
+                    selected = 4),
+        selectInput("state",
+                    label = "State",
+                    choices = states,
+                    selected = "TN",
+                    selectize = TRUE),
+        selectInput("procedure",
+                    label = "Medical Procedure",
+                    choices = procedure,
+                    selected = "101 - SEIZURES W/O MCC",
+                    selectize = TRUE),
+        uiOutput("secondSelection"),
+        # First tab content
+        fluidRow(
+          box(status = "primary",
+              dataTableOutput("opioid_providers_table"), width = 12)
+          )
+       )
     )
-  ))
-)
+  )
+))
